17e45e140949dd3f56f9f29285996e3e5f9b77e4
Your Name <you@example.com>
2016-04-22 20:32:03 -0400

    update

--- hw5/client.c
+++ hw5/client.c
@@ -65,7 +65,7 @@ int main (int argc, char ** argv) {
 
 	}
 
-	/*
+	/*		
 	if(login()<0){
 		errorPrint();
 		fprintf(stderr, "Login response failed\n" );
@@ -343,7 +343,7 @@ void listuHandler(char* buffer){
 		index++;
 	}
 
-	while(i<index){
+	while(i<(index-1)){
 		fprintf(stdout, "user: %s\n",temp[i]);
 		i=i+2;
 	}
@@ -390,7 +390,8 @@ void openChatHandler(char*buf){
 	  char* socket_path = "./socket";
 	  pid_t pid;
 	  int status = 0;
-	  char*  arguments[10];
+	  char *arguments[11];
+	  memset(arguments,0,sizeof(arguments));
 	  arguments[0]="xterm";
 	  arguments[1]="-hold";
 	  arguments[2]="-geometry";
@@ -399,7 +400,7 @@ void openChatHandler(char*buf){
 	  arguments[5]="Chat Room";
 	  arguments[6]="-e";
 	  arguments[7]="./chat";
-	  arguments[8]=NULL;
+	 // arguments[8]="0000000000000";
 	  arguments[9]=NULL;
 	 
 	  if ( (socketFD = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
@@ -421,12 +422,12 @@ void openChatHandler(char*buf){
 	  pid = fork();
 
 	  if(pid==0){
-	  		printf("in child\n" );
-	  		
+	  		printf("in child socket:%d \n",socketFD );
 	  		char temp[10];
+	  		memset(temp,0,10);
 	  		sprintf(temp,"%d",socketFD);
-	  		strcpy(arguments[8],temp);
-	  	
+	  		arguments[8]= temp;
+	  		
 	  		execvp(arguments[0],arguments);
 	  	//	exit(EXIT_SUCCESS);
 	  }else{
